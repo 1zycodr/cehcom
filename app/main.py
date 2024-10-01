a7e580a74ae3e49d53517b28aba626ea318ec2b8
lzycodr <muhitdinov.a3@wb.ru>
2024-10-01 15:43:51 +0500

    update

--- app/main.py
+++ app/main.py
@@ -10,8 +10,6 @@ from app.core.middleware import catch_exceptions_middleware
 from app.job.sync import sync_notion_amo, sync_notion_leads
 from app.api.v1.router import api_router as v1_router
 from app.repository.tgbot import Alert
-from app.services import NotionService
-from app.utils.file_storage import save_file_from_url
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
@@ -34,34 +32,33 @@ if settings.ENVIRONMENT != settings.Environment.local.value:
     app.middleware('http')(catch_exceptions_middleware)
 
 
-# @app.on_event('startup')
-# @repeat_every(seconds=60, wait_first=True)
-# def sync():
-#     if red.get('sync-running') is not None:
-#         print('sync already running')
-#         return
-#     red.set('sync-running', '1')
-#     sync_notion_amo()
-#
-#
-# @app.on_event('startup')
-# @repeat_every(seconds=10, wait_first=True)
-# def sync_leads():
-#     if red.get('sync-leads-running') is not None:
-#         print('sync leads already running')
-#         return
-#     red.set('sync-leads-running', '1')
-#     sync_notion_leads()
-#
-#
-# @app.on_event('shutdown')
-# def shutdown():
-#     Alert.critical('`🛑 Сервер остановлен.`')
+@app.on_event('startup')
+@repeat_every(seconds=60, wait_first=True)
+def sync():
+    if red.get('sync-running') is not None:
+        print('sync already running')
+        return
+    red.set('sync-running', '1')
+    sync_notion_amo()
+
+
+@app.on_event('startup')
+@repeat_every(seconds=10, wait_first=True)
+def sync_leads():
+    if red.get('sync-leads-running') is not None:
+        print('sync leads already running')
+        return
+    red.set('sync-leads-running', '1')
+    sync_notion_leads()
+
+
+@app.on_event('shutdown')
+def shutdown():
+    Alert.critical('`🛑 Сервер остановлен.`')
 
 
 if __name__ == "__main__":
-    NotionService.sync_with_amo()
-    # Alert.critical('`🟢 Сервер запущен.`')
-    # red.delete('sync-running')
-    # red.delete('sync-leads-running')
-    # uvicorn.run("app.main:app", host='0.0.0.0', port=8000)
+    Alert.critical('`🟢 Сервер запущен.`')
+    red.delete('sync-running')
+    red.delete('sync-leads-running')
+    uvicorn.run("app.main:app", host='0.0.0.0', port=8000)
